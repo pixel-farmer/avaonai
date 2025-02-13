3b715a38a67debee798e1f1f884fcf3325088ab2
pixel-farmer <cassie@pixel-farmers.com>
2025-02-13 17:42:09 -0500

    fixed leading

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
 <div className="mx-auto flex max-w-7xl items-center justify-center bg-transparent">
   <div className="w-[1247px] h-[475px] inline-flex items-center justify-center">
-  <p className="text-white text-5xl font-roboto font-medium leading-5 py-[50px] bg-transparent uppercase">
+  <p className="text-white text-5xl font-roboto font-medium leading-25 py-[50px] bg-transparent uppercase">
         Building better apps with ai
       </p>
   </div>
