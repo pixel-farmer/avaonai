515cf53e936abb4c40d8e650982e489a63c3a634
pixel-farmer <cassie@pixel-farmers.com>
2025-05-17 07:27:11 -0400

    new copy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 <div className="mx-auto flex max-w-7xl items-center justify-center bg-transparent">
 <div className="w-[1247px] h-[475px] flex flex-col items-left justify-center">
   <h1 className="text-white text-5xl font-roboto font-medium leading-25 py-[50px] bg-transparent uppercase">
-    I am AVA<br></br>AI Singer from a Virtual World
+    I am AVA — AI Singer<br></br>from a Virtual World
   </h1>
   <p className="text-white text-xl font-roboto font-medium leading-25">
     AI-crafted music. Cinematic virtual worlds.<br></br>
