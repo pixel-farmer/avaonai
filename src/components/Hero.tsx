ae339a7e83fefa6c6854440516d4ecd82b9e2803
pixel-farmer <cassie@pixel-farmers.com>
2025-05-17 06:59:19 -0400

    new copy

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,10 +6,10 @@ const Hero = () => {
 <div className="mx-auto flex max-w-7xl items-center justify-center bg-transparent">
 <div className="w-[1247px] h-[475px] flex flex-col items-left justify-center">
   <h1 className="text-white text-5xl font-roboto font-medium leading-25 py-[50px] bg-transparent uppercase">
-    Explore the Latest in<br></br> AI with AVA on AI
+    Meet the virtual world singer<br></br> I am AVA
   </h1>
   <p className="text-white text-xl font-roboto font-medium leading-25">
-    From cutting-edge AI tutorials to app showcases, dive into<br></br> the future of tech and creativity.
+    She mixes real lyrics with AI music and visuals from<br></br> the virtual world, Second Life.
   </p>
   <button className="w-[140px] h-[50px] mt-8 font-roboto font-medium bg-yellow-400 text-black rounded hover:bg-yellow-300">
     Start Exploring
