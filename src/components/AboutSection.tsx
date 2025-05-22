9c9520125c24a223b20ef08642731378a1da01ea
pixel-farmer <cassie@pixel-farmers.com>
2025-05-22 15:46:30 -0400

    many updates

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -5,7 +5,7 @@ const AboutSection = () => {
     <div className="w-full h-[500px] bg-white flex items-center justify-center">
     <div className="w-[1247px] h-[500px] flex flex-col items-left justify-center bg-[url('/about-bg.jpg')] bg-cover bg-center bg-no-repeat">
     <h1 className="ml-[520px] text-black text-5xl font-roboto font-medium leading-25 py-[50px] bg-transparent uppercase">
-      Who is I am AVA?
+      Who is AVA?
     </h1>
     <p className="w-[760px] ml-[520px] text-black text-xl font-roboto font-medium leading-25">
     "I am AVA" Meet AVA — a virtual artist at the intersection of music, storytelling, and machine-made dreams.
