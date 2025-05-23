27b796a640d1a150b718bd6eeee9daf6c4aee990
pixel-farmer <cassie@pixel-farmers.com>
2025-05-23 08:27:53 -0400

    many updates

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -8,8 +8,8 @@ const AboutSection = () => {
       Who is AVA?
     </h1>
     <p className="w-[760px] ml-[520px] text-black text-xl font-roboto font-medium leading-25">
-    "I am AVA" Meet AVA — a virtual artist at the intersection of music, storytelling, and machine-made dreams.
-    On this channel, AVA creates original music videos inside Second Life, fusing cinematic virtual worlds with AI-generated vocals, sound, and visuals. Every piece is a journey through the future of creativity.
+    "I am AVA" Meet AVA — a virtual artist at the intersection of music, storytelling, and machinima.
+    On her youTube channel, AVA creates original lyrics and music videos inside Second Life, fusing cinematic virtual worlds with AI-generated vocals and sounds. Every piece is a journey through the future of creativity.
     </p>
     </div>
   </div>
